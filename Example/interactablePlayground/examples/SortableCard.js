a747d37313b97c543662cf25e9d30b77ae566d53
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2018-10-09 14:46:05 +0100

    add base version of the sortable list

--- Example/interactablePlayground/examples/SortableCard.js
+++ Example/interactablePlayground/examples/SortableCard.js
@@ -1,48 +1,89 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import Interactable from '../../Interactable';
+import { FlatListSortable } from './utils/FlatListSortable';
 
-export default class SortableCard extends Component {
-  state = {
-    order: ['first', 'second', 'third'],
-  };
+let DATA = [
+  'James',
+  'Michael',
+  'Harold',
+  'Chloe',
+  'Bob',
+  'George',
+  'Samantha',
+  'Jenny',
+  'Sharon',
+];
+
+export default class SortableCard extends React.Component {
+  // state = {
+  //   order: ['first', 'second', 'third'],
+  // };
 
   render() {
     return (
       <View style={styles.container}>
-        {this.state.order.map(item => (
-          <Interactable.View
-            key={item}
-            horizontalOnly={true}
-            snapPoints={[{ x: 360 }, { x: 0, damping: 0.5 }, { x: -360 }]}>
-            <View style={styles.card}>
-              <Text style={styles.cardText}>{`${item}`}</Text>
-            </View>
-          </Interactable.View>
-        ))}
+        <FlatListSortable
+          onRowMoved={e => {
+            DATA.splice(e.to, 0, DATA.splice(e.from, 1)[0]);
+          }}
+          data={DATA}
+          renderItem={this.renderItem}
+          style={styles.list}
+        />
       </View>
     );
   }
+
+  renderItem = ({ item, index }) => (
+    // <Interactable.View key={item} snapPoints={[{ x: 0, damping: 0.5 }]}>
+    <TouchableOpacity style={styles.card}>
+      <TouchableOpacity style={styles.cardButton} onPress={this.onButtonPress}>
+        <Text style={{ textAlign: 'center' }}>Button</Text>
+      </TouchableOpacity>
+      <Text style={styles.cardText}>{`${item}`}</Text>
+    </TouchableOpacity>
+    // </Interactable.View>
+  );
+
+  onCardPress = () => {
+    // alert('Card was pressed');
+  };
+
+  onButtonPress = () => {
+    // alert('Card was pressed');
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: 'white',
   },
+  list: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
   card: {
-    width: 300,
-    height: 150,
+    marginVertical: 10,
+    marginHorizontal: 25,
+    paddingVertical: 10,
     backgroundColor: 'blue',
     borderRadius: 8,
-    marginVertical: 6,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
+    flexDirection: 'row',
   },
   cardText: {
     color: 'white',
     fontSize: 20,
+  },
+  cardButton: {
+    margin: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+    borderRadius: 8,
+    justifyContent: 'center',
+    backgroundColor: 'coral',
   },
 });
