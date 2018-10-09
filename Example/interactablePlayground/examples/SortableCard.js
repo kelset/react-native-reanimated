a0aff0b3d3952ffb9e4675e5ce5aa25e3e538c9b
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2018-10-09 15:20:31 +0100

    tempo comment

--- Example/interactablePlayground/examples/SortableCard.js
+++ Example/interactablePlayground/examples/SortableCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import Interactable from '../../Interactable';
+// import Interactable from '../../Interactable';
 import { FlatListSortable } from './utils/FlatListSortable';
 
 let DATA = [
